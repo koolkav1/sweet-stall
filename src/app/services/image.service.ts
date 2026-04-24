8d1e5ea32ccfd7a26a5de05cac8f1ce0b4f90b44
Kanwarjeet Khalsa <kenny.khalsa@gmail.com>
2026-04-24 11:58:13 +0100

    adding images

---
+++ src/app/services/image.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ImageService {
+    images: string[] = [
+        'https://storage.googleapis.com/candy-cart.firebasestorage.app/images%2FIMG_20260423_194744.webp',
+        'https://storage.googleapis.com/candy-cart.firebasestorage.app/images%2FIMG_20260423_194838.webp',
+        'https://storage.googleapis.com/candy-cart.firebasestorage.app/images%2FIMG_20260423_195232.webp',
+        'https://storage.googleapis.com/candy-cart.firebasestorage.app/images%2FIMG_20260423_200406.webp',
+        'https://storage.googleapis.com/candy-cart.firebasestorage.app/images%2FIMG_20260423_200508.webp',
+    ];
+}
